5228080f945a9cbc79d2072f21e8af431ed2bc91
riaz37 <riaz37.ipe@gmail.com>
2025-05-27 23:15:27 +0600

    fix: updated docker build

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,9 +1,9 @@
 // Export everything from the generated Prisma Client
-export * from '../node_modules/.prisma/client';
-export { PrismaClient, Prisma } from '../node_modules/.prisma/client';
+export * from '@prisma/client';
+export { PrismaClient, Prisma } from '@prisma/client';
 
 // Re-export specific enums for easier access
-export { UserRole, ProjectStatus, OrderStatus, NotificationType } from '../node_modules/.prisma/client';
+export { UserRole, ProjectStatus, OrderStatus, NotificationType } from '@prisma/client';
 
 // You can also add helper functions here
 export * from './helpers';
